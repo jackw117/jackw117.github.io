a6819b53379a1ee349a290e362c801c09fcc7d97
Jack Whitesell <jackw117@uw.edu>
2017-01-27 00:56:06 -0800

    commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//the javascript file for my Personal Portfolio page that uses Angular and Firebase
+//to display data on the html page
+
+var myApp = angular.module('myApp', ['ui.router', 'firebase'])
+
+.config(function($stateProvider, $locationProvider) {
+	$stateProvider
+    .state('home', {
+        url:'/',
+        templateUrl:'templates/front.html',
+    })
+	.state('about', {
+		url:'/about',
+		templateUrl:'templates/about.html',
+	})
+	.state('projects', {
+		url:'/projects',
+		templateUrl:'templates/projects.html',
+		controller:'ProjectsController',
+	});
+    $locationProvider.html5Mode(true);
+})
+
+//controller for the projects page
+.controller('ProjectsController', function($scope, $firebaseAuth, $firebaseArray, $firebaseObject) {
+	var ref = new Firebase('https://personal-portfolio117.firebaseio.com/');
+    var projectInfoRef = ref.child("projectInfo");
+    $scope.projectInfo = $firebaseArray(projectInfoRef);
+});
