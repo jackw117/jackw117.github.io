59992311f9b10855dfba85df00073f3a461fa5ce
jackw117 <jackw117@uw.edu>
2017-09-29 21:00:12 -0700

    names

--- js/names.js
+++ js/names.js
@@ -2,16 +2,16 @@ var myApp = angular.module('myApp', []);
 
 var myCtrl = myApp.controller('myCtrl', function($scope) {
 
-	$scope.membersArray = ["Jack Whitesell"];
+	$scope.membersArray = ["Adam Ku", "Steven Anderson", "Elizabeth Giacobbi", "Jason Waataja", "Sven Hansen", "Rushil Sharma"];
 
 	$scope.getNames = function() {
 		if ($scope.membersArray.length != 0) {
 			$scope.random = Math.floor(Math.random() * $scope.membersArray.length);
 			$scope.winner = $scope.membersArray[$scope.random];
-			var index = $scope.membersArray.indexOf($scope.membersArray[$scope.random]);
-			if (index > -1) {
-				$scope.membersArray.splice(index, 1);
-			}
+			//var index = $scope.membersArray.indexOf($scope.membersArray[$scope.random]);
+			//if (index > -1) {
+				$scope.membersArray.splice($scope.random, 1);
+			//}
 			console.log($scope.random);
 			console.log($scope.membersArray);
 		} else {
